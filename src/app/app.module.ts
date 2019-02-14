f1f80451389e8d01593266378854dd35b0027cf7
rodrigo <academiacp@gmail.com>
2019-02-14 14:26:06 -0300

    ttd proyect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TtdComponent } from './components/ttd/ttd.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { APP_ROUTING } from './app.router';
+import { WeComponent } from './components/we/we.component';
+import { SecretariaTecnicaComponent } from './components/secretaria-tecnica/secretaria-tecnica.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TtdComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    WeComponent,
+    SecretariaTecnicaComponent    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/secretaria-tecnica/secretaria-tecnica.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-secretaria-tecnica',
+  templateUrl: './secretaria-tecnica.component.html',
+  styleUrls: ['./secretaria-tecnica.component.css']
+})
+export class SecretariaTecnicaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
